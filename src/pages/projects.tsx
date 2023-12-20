bc6a4871189427493cdbefeb82866a398c111303
tj <thinupavani@gmail.com>
2023-12-20 06:32:09 +0530

    projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -16,24 +16,26 @@ type Props = {
 }
 const Projects = ({ projects }: Props) => {
 
-  console.log("projects: ", projects)
+  // console.log("projects: ", projects)
 
   useEffect(() => {
 
   }, []);
   return (
     <>
-      <div className="d-flex row row-cols-1 row-cols-md-2 row-cols-lg-3 p-5 align-items-center justify-content-center">
+    <div className="d-flex flex-column justify-content-center align-items center w-100 p-2 pb-0 pb-lg-5 p-5 py-5 mt-5 text-center">
+          <h1 className={`${styles.heroTitle2} mb-0`}><span>Projects</span></h1>
+          <p className={`${styles.heroPara}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ex beatae <br></br>autem eum veniam quod ea, iste libero velit dolorem?</p>
+    </div>
+      <div className={`d-flex row row-cols-1 row-cols-md-2 row-cols-lg-3 pb-2 py-lg-5 align-items-center justify-content-center ${styles.sideSpacing}`}>
         {
           projects.map((project, index) => {
-            return (
+            return ( 
               <div className="d-flex mb-4" key={index}>
                 <div className={`d-flex col flex-column position-relative  ${styles.cardWrapper}`} >
-                <Image src={`${project.thumbnail}`} alt="" width={500} height={300} className='img-fluid' />
-                <div className={`d-flex flex-column p-3 ${styles.CardContent} w-100 justify-content-end`}>
-                  <Link href={project.slug.current}>{project.title}</Link>
-                  <p className='mb-0'>{project.categories}</p>
-                </div>
+                <Link href={project.slug.current}>
+                  <Image src={`${project.thumbnail}`} alt="" width={500} height={300} className='img-fluid' />
+                </Link>
               </div>
               </div>
             )
